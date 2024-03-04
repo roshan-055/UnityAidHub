7e42fd1395bd31da121fe4ab763f6680b57f3ccf
roshan bhattarai <bhattarairoshan40@gmail.com>
2024-03-04 16:02:50 +0545

    email integration to send mail after donation to user

--- src/donations/donations.module.ts
+++ src/donations/donations.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { DonationsService } from './donations.service';
 import { DonationsController } from './donations.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
+    }),
+  ],
   controllers: [DonationsController],
   providers: [DonationsService],
 })
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,20 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     PrismaModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
+    }),
     MulterModule.register({
       dest: './images/posts',
     }),
